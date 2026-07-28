bbb7a61064700b67075e9205e3662693e563d343
Dishant Bundheliya <180743655+DM2712@users.noreply.github.com>
2026-07-28 22:09:35 +0530

    Time: 11 ms (76.03%), Space: 13.9 MB (40.32%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findErrorNums(self, nums):
+        seen = set()
+        duplicate = 0
+        missing = 0
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                missing = i
+                break
+
+        return [duplicate, missing]
